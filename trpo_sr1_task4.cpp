3afd28e1c806641a076c9deafe371cbfa20ef09b
polinaLesak <lesakpolina@gmail.com>
2021-09-30 10:14:42 +0300

    Create trpo_sr1_task4.cpp

---
+++ trpo_sr1_task4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int dl_p, sh_p, vis_p, dl_otv, sh_otv, vis_otv, v_p;
+	cout << "Введите длину основания параллелепипеда: " << endl;
+	cin >> dl_p;
+	cout << "Введите ширину основания параллелепипеда: " << endl;
+	cin >> sh_p;
+	cout << "Введите высоту параллелепипеда: " << endl;
+	cin >> vis_p;
+	cout << "Введите длину отверстия" << endl;
+	cin >> dl_otv;
+	cout << "Введите ширину отверстия: " << endl;
+	cin >> sh_otv;
+	if ((dl_p < dl_otv && sh_p < sh_otv) || (dl_p < sh_otv && sh_p < dl_otv ) || ( sh_p < sh_otv && vis_p < dl_otv) || (dl_p < sh_otv && vis_p < dl_otv) || (vis_p < sh_otv && dl_p < dl_otv ) || (vis_p < sh_otv && sh_p < dl_otv))
+	{
+		cout << "Поместится" << endl;
+	}
+	else
+	{
+		cout << "Не поместится" << endl;
+	}
+	return 0;
+
+}
