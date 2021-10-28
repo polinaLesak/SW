c92b1f0a532703dca12970bf377d685671ca93db
polinaLesak <lesakpolina@gmail.com>
2021-10-28 11:34:12 +0300

    max enght of repeting elements

--- task-2.cpp
+++ task-2.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int find_element();
-
+int max_lenght_of_repeting_elements();
 
 int main()
 {
@@ -23,6 +23,13 @@ int main()
 		case 1:
 			find_element();
 			cout << endl;
+			break;
+		case 2:
+			max_lenght_of_repeting_elements();
+			cout << endl;
+			break;
+		default:
+			cout << "Такого задания нет" << endl;
 			break;
 		}
 	} while (n != 0);
@@ -57,10 +64,27 @@ int find_element()
 	{
 		if (arr[i] == k)
 		{
-			cout << "Ваше число находится под индеском : [" << i  << "]\n"; break;
+			cout << "Ваше число находится под индеском : [" << i << "]\n"; break;
 		}
 		if (i == size_of_array)
 			cout << "Такого числа в последовательности нет" << endl;
 	}
+	return 0;
+}
+
+int max_lenght_of_repeting_elements()
+{
+	int size_of_array;
+	int* arr = entering_filling_oneDimensional_array(size_of_array);
+
+	for (int i = 3; i < size_of_array - 2; i++)
+	{
+		if (arr[i] < (arr[i - 2] - arr[i - 1]))
+		{
+			cout << "Число соответсвующее условию:  " << "[" << i + 1 << "]";
+			cout << arr[i] << "\n";
+		}
+	}
+
 	return 0;
 }
